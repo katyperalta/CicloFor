337284b43fc783c536b4715b944c51318d1b0486
ManageriMac <managerimac@SIN-IMAC03.local>
2012-11-21 15:17:06 -0700

    Initial Commit

---
+++ CicloFor/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CicloFor
+//
+//  Created by ManageriMac on 21/11/12.
+//  Copyright (c) 2012 ManageriMac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
